a2d9ff39e5f7f97617fcb921ab780832d6648d78
Vada-Cyber <gvadachkoria@gmail.com>
2025-09-30 14:33:50 +0400

    Added °C and °F symboles

--- temerature_converter.py
+++ temerature_converter.py
@@ -6,5 +6,7 @@ if temperature_celsius < 15:
 elif temperature_celsius >= 15 and temperature_celsius < 30:
     print ("Moderate Temperature")
 else:
-    print("Hot Temperature")
-print(f"temperature_fahrenheit is : {temperature_fahrenheit}")
+    print("Hot Temperature, It's Hot")
+print(f"temperature_fahrenheit is : {temperature_fahrenheit}°F")
+print(f"{temperature_celsius}°C  is : {temperature_fahrenheit}°F")
+

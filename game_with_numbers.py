d9311b59df5f514d9ed1ced68524851d85e17fb9
Vada-Cyber <gvadachkoria@gmail.com>
2025-10-01 06:10:55 +0400

    added ranges  low and high,  but close to the true value

--- game_with_numbers.py
+++ game_with_numbers.py
@@ -3,18 +3,22 @@ iterations = 0
 
 while True:
     iterations += 1
-    symple_value = int(input(" Input_value is: "))
-    print(f"Iteration {iterations}: Generated {symple_value}")
+    symple_value = int(input("\n Input_value is: "))
+    print(f"\n Iteration {iterations}: Generated {symple_value}")
     if symple_value == 50:
         print(f"\n✓ Found 50! It took {iterations} iterations.")
         break
-    elif symple_value < 50:
-        print(f"iterations : {iterations} , too_low")
+    elif symple_value >=40 and symple_value < 50:
+        print(f"\n iterations : {iterations} , You're_close, But_Low")
+    elif symple_value < 40 :
+        print(f"\n iterations : {iterations} , Too_low")
+    elif symple_value >50 and symple_value <= 60:
+        print(f"\n iterations : {iterations} , You're_close, But_High")
     else:
-        print(f"iterations: {iterations}. too_high")
-print(f"Total iterations: {iterations}")
+        print(f"\n iterations: {iterations}. Too_High")
+print(f"\n Total iterations: {iterations} ")
 if iterations == 1:
-    print ("You're Lucky")
+    print ("\n You're Lucky! \n")
 
 
 

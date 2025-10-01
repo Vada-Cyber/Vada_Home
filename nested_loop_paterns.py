d7a7cf5b090340574ec75e683ffc8d20f51cfd2d
Vada-Cyber <gvadachkoria@gmail.com>
2025-10-01 07:33:10 +0400

    no comments

--- nested_loop_paterns.py
+++ nested_loop_paterns.py
@@ -1,8 +1,6 @@
-print("Nested loops with break:")
-for i in range(3):
-    print(f"Outer loop: {i}")
-    for j in range(3):
-        if j == 1:
-            print(f"  Found j=1, breaking inner loop")
-            break
-        print(f"  Inner loop: {j}")
+print(" \nNested loops with break:")
+for i in range(6):
+    for j in range (6):
+        j = i * "*"
+        print(j)
+        break
